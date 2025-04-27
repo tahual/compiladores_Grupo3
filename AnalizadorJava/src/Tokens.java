d929970b2953043193094b15f2329844615ace6a
RazorXDK <mgrajeda@miumg.edu.gt>
2025-04-27 12:22:01 -0600

    Se agrego Dashboard

---
+++ AnalizadorJava/src/Tokens.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+
+/**
+ *
+ * @author ioch
+ */
+//se enumeran TODOS los tokens que seran utilizados
+public enum Tokens {
+    // Palabras reservadas específicas
+    
+    excepciones,
+    Reservadas,
+    Booleano,
+    entero,
+    si,
+    contrario,
+    mientras,
+    para,
+    hacer,
+    repetir,
+    hasta,
+    retornar,
+    booleano,
+    true_,
+    false_,
+
+    // Identificadores y literales
+    Identificador,
+    Numero,
+    Cadena,
+    Decimales,
+
+    // Operadores
+    opSuma,
+    opResta,
+    opMultiplicacion,
+    opDivision,
+    opIgual,
+    opIgualIgual,
+    opDiferente,
+    opMenor,
+    opMayor,
+    opMenorIgual,
+    opMayorIgual,
+    opAND,
+    opOR,
+    raiz,
+
+    // Símbolos
+    ParentesisApertura,
+    ParentesisCierre,
+    LlaveApertura,
+    LlaveCierre,
+    PuntoComa,
+    Coma,
+    Arroba,
+
+    // Otros
+    ERROR,
+    INICIO
+}
